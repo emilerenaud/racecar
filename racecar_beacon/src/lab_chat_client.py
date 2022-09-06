40865b4d1220496d8f3948b7c24646e80db1f287
mimil2014 <mimil2014@gmail.com>
2022-09-06 16:49:26 -0400

    lab_chat_client

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+from encodings import utf_8
 import socket
 
-HOST = '127.0.0.1'
+HOST = '10.42.0.211'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+def sendMessage(msg):
+    message = msg.encode(utf_8)
+
+s.send('disconnect'.encode('utf_8'))
+data = s.recv(1024)
+print(data)
+s.close()
