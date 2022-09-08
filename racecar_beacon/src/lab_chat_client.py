938406f4f9ae3f8ed0167b1d7dac8a0f6a390457
mimil2014 <mimil2014@gmail.com>
2022-09-08 14:12:06 -0400

    lab basic chat

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -3,16 +3,18 @@
 from encodings import utf_8
 import socket
 
-HOST = '10.42.0.211'
+HOST = '127.0.0.1'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 def sendMessage(msg):
-    message = msg.encode(utf_8)
+    message = msg.encode('utf_8')
+    s.send(message)
 
-s.send('disconnect'.encode('utf_8'))
+sendMessage("HelloWord")
 data = s.recv(1024)
 print(data)
+sendMessage("Hello 2")
 s.close()
--- racecar_beacon/src/lab_chat_server.py
+++ racecar_beacon/src/lab_chat_server.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python
 
+from ast import Break
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 65432
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
 
+def receive(conn):
+    while True:
+        data = conn.recv(1024)
+        if not data :
+            None
+        else:
+            print(data)
+    None
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORT))
+    s.listen(2)
+except:
+    print("erreur while bind or listen")
+
+(conn,addr) = s.accept()
+
+while True:
+    data = conn.recv(1024)
+
+    if  not data :
+        None
+    else:
+        print(data)
+        conn.send(data)
+    
+
+    
+
+conn.close()
