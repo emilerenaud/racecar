cad57fbe2a736378ed2fa3e0b767c1c5eaa5a23a
mimil2014 <mimil2014@gmail.com>
2022-09-10 13:05:13 -0400

    basic UDP working

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
 import socket
+import threading
+import time
 
 HOST = '127.0.0.1'
+
 # This process should listen to a different port than the RemoteRequest client.
 PORT = 65431
+
+if __name__=="__main__":
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    client.bind(("", 37020))
+    while True:
+        # Thanks @seym45 for a fix
+        data, addr = client.recvfrom(1024)
+        print("received message: %s"%data)
 
