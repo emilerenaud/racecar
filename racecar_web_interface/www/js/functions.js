58978ca149c7ae0309abc8e3ba205f5328673f99
mimil2014 <mimil2014@gmail.com>
2022-10-16 17:28:38 -0400

    vehicule bouge partout

--- racecar_web_interface/www/js/functions.js
+++ racecar_web_interface/www/js/functions.js
@@ -1,9 +1,48 @@
 
 
-function buttonEvent(text,state)
+function buttonEvent(idButton)
 {
-  writeConsole(text);
-  updateTwist(state);
+    console.log(idButton);
+    switch(idButton)
+    {
+        case 'LFront':
+            writeConsole('Avant Gauche<br>');
+            updateTwist(1,1);
+            break;
+        case 'Front':
+            writeConsole('Avant<br>');
+            updateTwist(1,0);
+            break;
+        case 'RFront':
+            writeConsole('Avant Droite<br>');
+            updateTwist(1,-1);
+            break;
+        case 'Left':
+            writeConsole('Gauche<br>');
+            updateTwist(0,1);
+            break;
+        case 'Stop':
+            writeConsole('Stop<br>');
+            updateTwist(0,0);
+            break;
+        case 'Right':
+            writeConsole('Droite<br>');
+            updateTwist(0,-1);
+            break;
+        case 'LBack':
+            writeConsole('Arrière Gauche<br>');
+            updateTwist(-1,1);
+            break;
+        case 'Back':
+            writeConsole('Arrière<br>');
+            updateTwist(-1,0);
+            break;
+        case 'RBack':
+            writeConsole('Arrière Droite<br>');
+            updateTwist(-1,-1);
+            break;
+
+    }
 }
 
 function writeConsole(text)
@@ -19,12 +58,10 @@ function clearConsole()
   document.getElementById('consoleLog').innerHTML = '';
 }   
 
-function updateTwist(state)
+function updateTwist(linear,angular)
 {
-    if(state == 1 || state ==0)
-    {
-        twist.linear.x = state;
-    }
+    twist.linear.x = linear;
+    twist.angular.z = angular;
 }
 
 function disableControlButtons(state)
