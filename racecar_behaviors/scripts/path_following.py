9174049004e039657998a0430772520f523227ca
mimil2014 <mimil2014@gmail.com>
2022-12-04 13:38:00 -0500

    seems to work

--- racecar_behaviors/scripts/path_following.py
+++ racecar_behaviors/scripts/path_following.py
@@ -25,7 +25,7 @@ class PathFollowing:
 
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.current_angle = 0
-
+        self.current_posX = 0
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
         self.balloon_sub = rospy.Subscriber('position_balloon',Float32MultiArray, self.balloon_callback, queue_size=10)
@@ -33,13 +33,14 @@ class PathFollowing:
         # Camera shit
         self.balloon_list = list()
         self.bridge = CvBridge()    
-        self.balloon_path = r'/home/jackson/racecar/debris'
+        self.balloon_path = r'/home/emile/racecar/debris'
 
         # Goals
-        self.goal_start = self.create_goal(0, 0, np.pi, "pos") # WHY np.pi?
+        self.goal_start = self.create_goal(0, 0, np.pi, "pos") 
         self.goal_end = self.create_goal(13.5, 2.1, 0, "pos")
         self.goal_end_r = self.create_goal(13.5, 2.1, np.pi, "pos")
         self.goals = [self.goal_end, self.goal_end_r, self.goal_start]
+        print(self.goals)
 
         # Action clients
         self.mb_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
@@ -51,20 +52,28 @@ class PathFollowing:
         if len(self.goals) > 0:
             self.mb_client.send_goal(self.goals[0][0], self.goal_reached_callback)
 
-    def goal_reached_callback(self):
+    def goal_reached_callback(self,goal_status, result):
+        # for goal in self.goals:
+        #     print(goal.target_pose.pose.position.x)
+        #     print(goal.target_pose.pose.position.y)
         goal_x = self.goals[0][0].target_pose.pose.position.x
         goal_y = self.goals[0][0].target_pose.pose.position.y
         goal_angle = quaternion_to_yaw(self.goals[0][0].target_pose.pose.orientation)
-        # rospy.loginfo("Goal reached!\nX : {0}\nY : {1}\nAngle : {2}\n".format(goal_x, goal_y, goal_angle))
+        rospy.loginfo("Goal reached!\nX : {0}\nY : {1}\nAngle : {2}\n".format(goal_x, goal_y, goal_angle))
 
         if (self.goals[0][1] == "balloon"):
-
-            self.create_goal(goal_x, goal_y, goal_angle + self.balloon_sub.data[3], "picture")
+            self.goals.pop(0)
+            msg_balloon = rospy.wait_for_message('position_balloon',Float32MultiArray)
+            goal = self.create_goal(goal_x, goal_y, self.current_angle + msg_balloon.data[3], "picture")
+            self.goals.insert(0,goal)
 
         elif( self.goals[0][1] == "picture" ):
             self.take_picture()
+            self.goals.pop(0)
+        else:
+            self.goals.pop(0)
             
-        self.goals.pop(0)
+        # self.goals.pop(0)
         self.send_goal()
 
     def scan_callback(self, msg):
@@ -77,11 +86,13 @@ class PathFollowing:
         twist.linear.x = self.max_speed
         twist.angular.z = 0
            
-        self.cmd_vel_pub.publish(twist)
+        # self.cmd_vel_pub.publish(twist)
         
     def odom_callback(self, msg):
         # rospy.loginfo("Current speed = %f m/s", msg.twist.twist.linear.x)
         self.current_angle = quaternion_to_yaw(msg.pose.pose.orientation)
+        # print(self.current_angle)
+        self.current_posX = msg.pose.pose.position.x
         pass
 
     def balloon_callback(self,msg):
@@ -95,31 +106,40 @@ class PathFollowing:
         new_balloon = True
 
         # Verifier si on a deja enregristrer cette position
-        if(len(self.balloon_list) == 0):
-            self.balloon_list.append((pos_x,pos_y))
-            rospy.loginfo("FIRST BALloON")
-        else:
-            
+        # if(len(self.balloon_list) == 0):
+            # self.balloon_list.append((pos_x,pos_y))
+            # rospy.loginfo("FIRST BALloON")
+        # else:
+        if distance <=3.5:
             for balloon in self.balloon_list:
                 if (math.hypot((abs(pos_x - balloon[0])),(abs(pos_y - balloon[1]))) <= tolerance):
                     new_balloon = 0
                     break
 
-        if new_balloon:
-            rospy.loginfo("New ball")
-            self.balloon_list.append((pos_x,pos_y))
+            if new_balloon:
+                rospy.loginfo("New ball")
+                self.balloon_list.append((pos_x,pos_y))
 
-            angle = msg.orientation.z
-            distance = 1.5 # From the blob to take the picture
-            goal_x = pos_x - distance * np.cos(self.current_angle + angle)
-            goal_y = pos_y - distance * np.sin(self.current_angle + angle)
-            goal = self.create_goal(goal_x, goal_y, self.current_angle + angle, "balloon")
+                # angle = msg.orientation.z
+                distance = 1.8 # From the blob to take the picture
+                if self.current_posX > pos_x:
+                    goal_x = pos_x + distance
+                else:
+                    goal_x = pos_x - distance
+                
+                # goal_x = pos_x - distance * np.cos(self.current_angle + angle)
+                
+                goal_y = pos_y #- distance * np.sin(self.current_angle + angle)
+                goal = self.create_goal(goal_x, goal_y, self.current_angle + angle, "balloon")
+                print(goal_x)
+                print(goal_y)
+                print(self.current_angle)
+                print(angle)
+                self.goals.insert(0, goal)
+                self.send_goal()
 
-            self.goals.insert(0, goal)
-            self.send_goal()
 
-
-            self.take_picture()
+            # self.take_picture()
            
             
             # Se rendre au debris si nouveau debris
